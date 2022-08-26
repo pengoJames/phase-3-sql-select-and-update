b5c2a6817e690979f64790afb39ab872c5a2e9d7
Pe-ngo <jamespengo95@gmail.com>
2022-08-26 16:26:00 +0300

    SQL CRUD

---
+++ 01_insert_cats_into_cats_table.sql
@@ -0,0 +1,6 @@
+INSERT INTO cats (name, age, breed) VALUES ('Maru', 3, 'Scottish Fold');
+
+INSERT INTO cats(name, age, breed) VALUES ('Lil Bub', 5, 'American Shorthair');
+
+INSERT INTO cats(name, age, breed) VALUES ('Hannah', 1, 'Tabby');
+
